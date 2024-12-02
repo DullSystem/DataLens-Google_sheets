e7231ba6833bfde2ca7f5390577d3a9b0913d69c
Sergey Krasnoperov <94071620+DullSystem@users.noreply.github.com>
2024-12-02 14:50:25 +0300

    Add files via upload

---
+++ api_fields.py
@@ -0,0 +1,37 @@
+hit_field_list = [
+    "ym:pv:date",
+    "ym:pv:dateTime",
+    "ym:pv:URL",
+    "ym:pv:deviceCategory",
+    "ym:pv:operatingSystemRoot",
+    "ym:pv:clientID",
+    "ym:pv:browser",
+    "ym:pv:lastTrafficSource",
+    "ym:pv:goalsID",
+    "ym:pv:parsedParamsKey1",
+    "ym:pv:parsedParamsKey2"
+]
+
+visit_field_list = [
+    "ym:s:date",
+    "ym:s:dateTime",
+    "ym:s:pageViews",
+    "ym:s:visitDuration",
+    "ym:s:deviceCategory",
+    "ym:s:operatingSystemRoot",
+    "ym:s:clientID",
+    "ym:s:browser",
+    "ym:s:browserEngine",
+    "ym:s:watchIDs",
+    "ym:s:regionCity",
+    "ym:s:regionCityID",
+    "ym:s:startURL",
+    "ym:s:endURL",
+    "ym:s:referer",
+    "ym:s:lastTrafficSource",
+    "ym:s:purchaseRevenue",
+    "ym:s:purchaseID",
+    "ym:s:bounce",
+    "ym:s:isNewUser",
+    "ym:s:goalsID"
+]
